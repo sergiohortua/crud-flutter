2c70fc20737210be7766051cfac3fd95f1b1f6cd
Sergio Hortua <sergioandresjsv@gmail.com>
2023-12-04 20:42:32 -0500

    data in textField without edit

--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -3,32 +3,98 @@ import 'package:flutter/material.dart';
 
 class Details extends StatelessWidget {
   Person person;
+  final TextEditingController _textEditingControllerName =
+      TextEditingController();
+  final TextEditingController _textEditingControllerDescription =
+      TextEditingController();
+  final TextEditingController _textEditingControllerAge =
+      TextEditingController();
+
   Details(this.person, {super.key});
 
   @override
   Widget build(BuildContext context) {
+    _textEditingControllerName.text = person.name;
+    _textEditingControllerDescription.text = person.description;
+    _textEditingControllerAge.text = person.age.toString();
     return Scaffold(
       appBar: AppBar(
-        title: Text('Detalles'),
+        title: const Text('Detalles'),
       ),
-      body: Center(
-        child: Container(
-          child: Column(
-            children: [
-              Container(
-                child: Text('Foto'),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Text('Nombre: ${person.name}'),
-              SizedBox(height: 10.0),
-              Text('Description: ${person.description}'),
-              SizedBox(height: 10.0),
-              Text('Age: ${person.age}'),
-              SizedBox(height: 10.0),
-            ],
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: ListView(
+          children: [
+            Column(
+              // mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  width: 80.0,
+                  height: 80.0,
+                  decoration: const BoxDecoration(
+                    color: Colors.blue,
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage('assets/astro.jpg'),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('Nombre:'),
+                    const SizedBox(
+                      width: 30.0,
+                    ),
+                    SizedBox(
+                        width: 150.0,
+                        child: TextField(
+                          controller: _textEditingControllerName,
+                          onChanged: (value) => {print(value)},
+                        )),
+                  ],
+                ),
+                const SizedBox(height: 10.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('Description:'),
+                    const SizedBox(
+                      width: 10.0,
+                    ),
+                    SizedBox(
+                        width: 150.0,
+                        child: TextField(
+                          controller: _textEditingControllerDescription,
+                          onChanged: (value) => {print(value)},
+                        )),
+                  ],
+                ),
+                const SizedBox(height: 10.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('Age:'),
+                    const SizedBox(
+                      width: 60.0,
+                    ),
+                    SizedBox(
+                        width: 150.0,
+                        child: TextField(
+                          controller: _textEditingControllerAge,
+                          onChanged: (value) => (print(value)),
+                        )),
+                  ],
+                ),
+                const SizedBox(height: 10.0),
+              ],
+            ),
+          ],
         ),
       ),
     );
